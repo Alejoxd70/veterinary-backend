bcdaae9d7ff7df19b0ebcfc01fd091687e4e9bbb
Alejo <scastaneda578@gmail.com>
2025-04-25 09:49:11 -0500

    relational-model-completed

--- prisma/migrations/20250422215828_add_pets_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Pet" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    "breed" TEXT NOT NULL,
-    "description" TEXT,
-    "isAdopted" BOOLEAN NOT NULL
-);
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,5 @@
 import { Router } from 'express'
 import { getAllUsers, registerUser, loginUser } from '../controllers/usersController.js'
-import { verifyAuth } from '../middleware/auth.js'
-import { verifyPermission } from '../middleware/verifyPermission.js'
-import { PERMISSIONS } from '../config/constants.js'
 
 const router = Router()
 
@@ -10,8 +7,9 @@ router.get('/users', getAllUsers)
 router.post('/users/register', registerUser)
 router.post('/users/login', loginUser)
 
-router.get('/admin', verifyAuth, verifyPermission({ permission: PERMISSIONS.ADMIN }), (req, res) => {
-  res.status(200).json({ message: 'Admin access granted' })
-})
+// Example of how to use verifyAuth and verifyPermission middleware
+// router.get('/admin', verifyAuth, verifyPermission({ permission: PERMISSIONS.ADMIN }), (req, res) => {
+//   res.status(200).json({ message: 'Admin access granted' })
+// })
 
 export default router
